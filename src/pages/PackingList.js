98f0b28f3fbb4ad4f7613b6ac28991ebb0bedaa4
hyeon-z <phj8221@naver.com>
2023-07-20 16:18:47 +0900

    feat: 패킹리스트 조회 페이지 구현

---
+++ src/pages/PackingList.js
@@ -0,0 +1,40 @@
+import React, {useEffect} from 'react';
+import {Link} from 'react-router-dom';
+
+function PackingList({packingLists}) {
+    useEffect(() => {
+    }, []);
+
+
+    return (
+        <div>
+            <div className="container mt-4">
+                <h2>패킹 리스트</h2>
+                <table className="table table-bordered table-striped">
+                    <thead>
+                    <tr>
+                        <th>리스트 명</th>
+                        <th>출발 날짜</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {packingLists.map((packingList) => (
+                        <tr key={packingList.id}>
+                            <td>{packingList.title}</td>
+                            <td>{packingList.departureDate}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+
+                <div className="d-grid gap-2 d-md-flex justify-content-md-end mt-4">
+                    <Link to="/packingList/create" className="btn btn-primary me-md-2">
+                        추가
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PackingList;
