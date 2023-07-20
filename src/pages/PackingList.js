9b12c635ec59ad1d435b8eb3dea17a81cf3a7b99
hyeon-z <phj8221@naver.com>
2023-07-21 03:49:43 +0900

    feat: 패킹리스트 상세정보와 짐 정보 가져와서 출력

--- src/pages/PackingList.js
+++ src/pages/PackingList.js
@@ -4,6 +4,7 @@ import {useParams} from 'react-router-dom';
 function PackingList() {
     const {id} = useParams();
     const [packingList, setPackingList] = useState(null);
+    const [packingItems, setPackingItems] = useState([]);
 
     useEffect(() => {
         fetch(`http://localhost:8080/api/v1/packingList/${id}`)
@@ -12,9 +13,23 @@ function PackingList() {
                 setPackingList(data);
             })
             .catch((error) => console.log(error));
+
+        fetch(`http://localhost:8080/api/v1/pack?packingListId=${id}`)
+            .then((response) => response.json())
+            .then((data) => {
+                setPackingItems(data);
+            })
+            .catch((error) => console.log(error));
     }, [id]);
 
-    if (!packingList) {
+    const handleCheckBoxChange = (itemId) => {
+        const updatedPackingItems = packingItems.map((item) =>
+            item.id === itemId ? {...item, checked: !item.checked} : item
+        );
+        setPackingItems(updatedPackingItems);
+    };
+
+    if (!packingList || packingItems.length === 0) {
         return <div>Loading...</div>;
     }
 
@@ -23,6 +38,33 @@ function PackingList() {
             <div className="container mt-4">
                 <h2>{packingList.title}</h2>
                 <p>출발 날짜: {packingList.departureDate}</p>
+                <p>설명: {packingList.description}</p>
+
+                <h2>패킹 리스트</h2>
+                <table className="table table-bordered table-hover">
+                    <thead>
+                    <tr>
+                        <th>카테고리</th>
+                        <th>짐 이름</th>
+                        <th>챙김 여부</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {packingItems.map((item) => (
+                        <tr key={item.id}>
+                            <td>{item.category}</td>
+                            <td>{item.name}</td>
+                            <td>
+                                <input
+                                    type="checkbox"
+                                    checked={item.checked}
+                                    onChange={() => handleCheckBoxChange(item.id)}
+                                />
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
             </div>
         </div>
     );
