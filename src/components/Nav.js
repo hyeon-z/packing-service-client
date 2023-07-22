20155cfb3a4f4dcd4cc2f208a11c638a4117eb1d
hyeon-z <phj8221@naver.com>
2023-07-23 04:12:33 +0900

    refacor: 사용하지 않는 Navbar 페이지 링크 제거

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -18,13 +18,6 @@ function Nav() {
                 >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link to="/packingList/all" className="nav-link active">모든 패킹리스트</Link>
-                        </li>
-                    </ul>
-                </div>
             </div>
         </nav>
     );
