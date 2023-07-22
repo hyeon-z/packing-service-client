39ccdb84b0dc9ad3617a93875e4cae9a1af04cd6
hyeon-z <phj8221@naver.com>
2023-07-23 04:11:36 +0900

    refactor: 홈화면 버튼 style 수정
    배경 이미지, 폰트 적용 및 정가운데 위치

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import '../App.css';
 
 function Home() {
     return (
-        <div>
-            <Link to="/packingList/all" className="btn btn-primary btn-lg">패킹 리스트 보기</Link>
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                backgroundImage: "url('/홈화면.jpg')",
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                fontFamily: 'CustomFontBold, sans-serif',
+            }}
+        >
+            <Link
+                to="/packingList/all"
+                className="btn btn-info btn-lg"
+            >
+                짐 싸러 GOGO!
+            </Link>
         </div>
     );
 }
