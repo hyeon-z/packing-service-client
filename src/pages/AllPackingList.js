87b35a001a20dc5d2af3ce29958422407852cf38
hyeon-z <phj8221@naver.com>
2023-07-22 18:56:09 +0900

    feat: 작업칸 누르면 패킹리스트 수정, 삭제 모달 띄우기

--- src/pages/AllPackingList.js
+++ src/pages/AllPackingList.js
@@ -1,12 +1,31 @@
 import React, {useState} from 'react';
-import {Button} from 'reactstrap';
-import CreatePackingList from './CreatePackingList';
+import {Button, Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
 
 function AllPackingList({packingLists}) {
     const [showModal, setShowModal] = useState(false);
+    const [selectedPackingList, setSelectedPackingList] = useState(null);
 
     const handleRowClick = (id) => {
         window.location.href = `/packingList/${id}`;
+    };
+
+    const handleEditClick = () => {
+        setShowModal(false);
+        // TODO: 수정 기능 구현
+    };
+
+    const handleDeleteClick = () => {
+        setShowModal(false);
+        // TODO: 삭제 기능 구현
+    };
+
+    const handleCellClick = (event, packingList) => {
+        const target = event.target;
+        const isCellClick = target.tagName.toLowerCase() === 'td';
+        if (isCellClick) {
+            setSelectedPackingList(packingList);
+            setShowModal(true);
+        }
     };
 
     return (
@@ -18,13 +37,21 @@ function AllPackingList({packingLists}) {
                     <tr>
                         <th>리스트 명</th>
                         <th>출발 날짜</th>
+                        <th>작업</th>
                     </tr>
                     </thead>
                     <tbody>
                     {packingLists.map((packingList) => (
-                        <tr key={packingList.id} onClick={() => handleRowClick(packingList.id)}>
-                            <td>{packingList.title}</td>
-                            <td>{packingList.departureDate}</td>
+                        <tr key={packingList.id}>
+                            <td onClick={() => handleRowClick(packingList.id)} style={{cursor: 'pointer'}}>
+                                {packingList.title}
+                            </td>
+                            <td onClick={() => handleRowClick(packingList.id)} style={{cursor: 'pointer'}}>
+                                {packingList.departureDate}
+                            </td>
+                            <td onClick={(e) => handleCellClick(e, packingList)} style={{cursor: 'pointer'}}>
+                                :
+                            </td>
                         </tr>
                     ))}
                     </tbody>
@@ -36,14 +63,24 @@ function AllPackingList({packingLists}) {
                     </Button>
                 </div>
             </div>
-
             {showModal && (
-                <CreatePackingList
-                    show={showModal}
-                    onClose={() => {
-                        setShowModal(false);
-                    }}
-                />
+                <Modal isOpen={showModal} toggle={() => setShowModal(false)}>
+                    <ModalHeader toggle={() => setShowModal(false)}>패킹리스트 작업 선택</ModalHeader>
+                    <ModalBody>
+                        <p>수정 또는 삭제를 선택하세요.</p>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={handleEditClick}>
+                            수정
+                        </Button>{' '}
+                        <Button color="danger" onClick={handleDeleteClick}>
+                            삭제
+                        </Button>{' '}
+                        <Button color="secondary" onClick={() => setShowModal(false)}>
+                            취소
+                        </Button>
+                    </ModalFooter>
+                </Modal>
             )}
         </div>
     );
